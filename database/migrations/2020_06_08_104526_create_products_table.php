61545ee898dcdf087d3b3a688c2b9a0aebe67ac9
Ganesh Khatri <kh6ganesh@gmail.com>
2020-06-08 18:11:08 +0545

    products and reviews tables seeds using faker

--- database/migrations/2020_06_08_104526_create_products_table.php
+++ database/migrations/2020_06_08_104526_create_products_table.php
@@ -18,7 +18,7 @@ class CreateProductsTable extends Migration
             $table->string('name');
             $table->text('detail');
             $table->integer('price');
-            $table->integer('stocl');
+            $table->integer('stock');
             $table->integer('discount');
             $table->timestamps();
         });
